d29377e46b3e8216c1dfca1fadecacfc7dd7b7eb
anaufal1 <37364779+anaufal1@users.noreply.github.com>
2019-05-12 21:30:25 +0700

    Update crontab.c

--- crontab.c
+++ crontab.c
@@ -8,6 +8,27 @@
 #include <syslog.h>
 #include <string.h>
 
+
+//fungsi bash
+void* run_command(void *arg)
+{
+  char *dir;
+  dir = (char *) arg;
+  signal(SIGCHLD, SIG_IGN);
+  pid_t pid, sid;
+  pid = fork();
+  if (pid == 0) {
+    sid = setsid();
+
+    if (sid < 0) {
+      exit(EXIT_FAILURE);
+    }
+
+    execlp("bash", "bash", dir, (char *)NULL);
+  }
+}
+
+
 int main() {
   pid_t pid, sid;
 
